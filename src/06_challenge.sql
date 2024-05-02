198e7660e61e00f2952c6d8414fb4432da882726
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-05-02 12:31:01 -0300

    Requisito 7

--- src/06_challenge.sql
+++ src/06_challenge.sql
@@ -1,14 +1,11 @@
--- Ativar a usuária Andressa
 UPDATE users
 SET active = true
 WHERE full_name = 'Andressa';
 
--- Desativar o usuário Rogério
 UPDATE users
 SET active = false
 WHERE full_name = 'Rogério';
 
--- Alterar o plano ativo da usuária Camila para o plano Mensal
 UPDATE users
 SET plan_id = 2
 WHERE full_name = 'Camila';
